93da026f0bd1406227fdba626f983db1884e8032
Quinn McCarthy Beaver <quinn.mccarthybeaver@gmail.com>
2022-05-03 16:18:31 -0400

    parameterized a lot of the fiddly numbers

--- src/main.py
+++ src/main.py
@@ -3,26 +3,15 @@ import argparse
 import SimpleITK as sitk
 
 def main():
-    # parse arguments
-    parser = argparse.ArgumentParser(
-        description="powerhouse segmentation image processing"
-    )
-    parser.add_argument(
-        "--viewer",
-        "-v",
-        nargs="?",
-        help='path to image viewer, default value is "Fiji"',
-        default="Fiji",
-    )
-    parser.add_argument(
-        "images", metavar="IMG", nargs="+", help="image filename(s) to process"
-    )
-    args = parser.parse_args()
+    args = getArgs()
+    medianRadius = args.median
+    topHatRadius = args.tophat_radius
+    minSize = args.minsize
 
     images = getImages(args.images)
 
     viewer = sitk.ImageViewer()
-    #viewer.SetApplication(args.viewer)
+    viewer.SetApplication(args.viewer)
 
     # notes: use aggressive median filter to remove noise
     # structures of interest are the most dense, so remove things of lower density
@@ -34,23 +23,76 @@ def main():
         # img = sitk.BinaryThreshold(img, lowerThreshold=180, upperThreshold=255, outsideValue=0)
         # img = sitk.ConnectedComponent(img)
 
-        img = sitk.Median(img_orig, radius=(7, 7))
+        img = sitk.Median(img_orig, radius=(medianRadius, medianRadius)) # param
         #img = sitk.SmoothingRecursiveGaussian(img_orig, sigma=10)
         img = sitk.LaplacianSharpening(img)
-        img = sitk.WhiteTopHat(img, kernelRadius=(10, 10))
+        img = sitk.WhiteTopHat(img, kernelRadius=(topHatRadius, topHatRadius)) # param
         img = sitk.OtsuThreshold(img, 0, 1)
         img = sitk.ConnectedComponent(img)
 
-        img_labelMap = sitk.RelabelComponent(img, minimumObjectSize=600)
+        img_labelMap = sitk.RelabelComponent(img, minimumObjectSize=minSize) # param
 
-        img_rgb = sitk.LabelToRGB(img)
+        overlay  = sitk.LabelMapOverlay(sitk.Cast(img_labelMap, sitk.sitkLabelUInt32), img_orig, opacity=.85)
+        labels = sitk.LabelToRGB(img)
 
         # print(img_rgb.GetPixelIDTypeAsString())
         # print(img_orig.GetPixelIDTypeAsString())
 
-        img_overlay = sitk.LabelMapOverlay(sitk.Cast(img_labelMap, sitk.sitkLabelUInt32), img_orig, opacity=.85)
 
-        viewer.Execute(img_overlay)
+        viewer.Execute(overlay)
+        viewer.Execute(labels)
+
+def getArgs():
+    # parse arguments
+    parser = argparse.ArgumentParser(
+        description="powerhouse segmentation image processing"
+    )
+
+    # Users choice of image viewer
+    parser.add_argument(
+        "--viewer",
+        "-v",
+        nargs="?",
+        help='path to image viewer, default value is "Fiji"',
+        default="Fiji",
+    )
+
+    # List of image files to process
+    parser.add_argument(
+        "images", metavar="IMG", nargs="+", help="image filename(s) to process"
+    )
+
+    # Median Radius
+    parser.add_argument(
+        "--median",
+        "-m",
+        nargs="?",
+        help="radius of the structuring element used in the median filter",
+        type=int,
+        default=7,
+    )
+
+    # White Top Hat Radius
+    parser.add_argument(
+        "--tophat_radius",
+        "-t",
+        nargs="?",
+        help="radius of the structuring element used in the White TopHat algorithm",
+        type=int,
+        default=10,
+    )
+
+    # Minimum size of the final labels
+    parser.add_argument(
+        "--minsize",
+        "-s",
+        nargs="?",
+        help="remove any labels that are below the specified size",
+        type=int,
+        default=600,
+    )
+
+    return parser.parse_args()
 
 
 
